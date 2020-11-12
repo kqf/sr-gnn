1e5a22573d771d86e9751560c60e7d0bbd0ef0d8
Sasha <q.dev.null@gmail.com>
2020-11-12 14:55:18 +0100

    Update the readme

--- model/utils.py
+++ model/utils.py
@@ -54,6 +54,33 @@ def split_validation(train_set, valid_portion):
     return (train_set_x, train_set_y), (valid_set_x, valid_set_y)
 
 
+def batch(inputs, mask, targets):
+    items, n_node, A, alias_inputs = [], [], [], []
+    for u_input in inputs:
+        n_node.append(len(np.unique(u_input)))
+    max_n_node = np.max(n_node)
+    for u_input in inputs:
+        node = np.unique(u_input)
+        items.append(node.tolist() + (max_n_node - len(node)) * [0])
+        u_A = np.zeros((max_n_node, max_n_node))
+        for i in np.arange(len(u_input) - 1):
+            if u_input[i + 1] == 0:
+                break
+            u = np.where(node == u_input[i])[0][0]
+            v = np.where(node == u_input[i + 1])[0][0]
+            u_A[u][v] = 1
+        u_sum_in = np.sum(u_A, 0)
+        u_sum_in[np.where(u_sum_in == 0)] = 1
+        u_A_in = np.divide(u_A, u_sum_in)
+        u_sum_out = np.sum(u_A, 1)
+        u_sum_out[np.where(u_sum_out == 0)] = 1
+        u_A_out = np.divide(u_A.transpose(), u_sum_out)
+        u_A = np.concatenate([u_A_in, u_A_out]).transpose()
+        A.append(u_A)
+        alias_inputs.append([np.where(node == i)[0][0] for i in u_input])
+    return alias_inputs, A, items, mask, targets
+
+
 class Data():
     def __init__(self, data, shuffle=False, graph=None):
         inputs = data[0]
@@ -82,27 +109,4 @@ class Data():
 
     def get_slice(self, i):
         inputs, mask, targets = self.inputs[i], self.mask[i], self.targets[i]
-        items, n_node, A, alias_inputs = [], [], [], []
-        for u_input in inputs:
-            n_node.append(len(np.unique(u_input)))
-        max_n_node = np.max(n_node)
-        for u_input in inputs:
-            node = np.unique(u_input)
-            items.append(node.tolist() + (max_n_node - len(node)) * [0])
-            u_A = np.zeros((max_n_node, max_n_node))
-            for i in np.arange(len(u_input) - 1):
-                if u_input[i + 1] == 0:
-                    break
-                u = np.where(node == u_input[i])[0][0]
-                v = np.where(node == u_input[i + 1])[0][0]
-                u_A[u][v] = 1
-            u_sum_in = np.sum(u_A, 0)
-            u_sum_in[np.where(u_sum_in == 0)] = 1
-            u_A_in = np.divide(u_A, u_sum_in)
-            u_sum_out = np.sum(u_A, 1)
-            u_sum_out[np.where(u_sum_out == 0)] = 1
-            u_A_out = np.divide(u_A.transpose(), u_sum_out)
-            u_A = np.concatenate([u_A_in, u_A_out]).transpose()
-            A.append(u_A)
-            alias_inputs.append([np.where(node == i)[0][0] for i in u_input])
-        return alias_inputs, A, items, mask, targets
+        return batch(inputs, mask, targets)
