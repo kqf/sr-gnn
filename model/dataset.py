22beac9bb2ce92eddb94d107973bfabc1d69b67d
Sasha <q.dev.null@gmail.com>
2020-11-12 12:44:18 +0100

    Add the boilerplate code for datasets

---
+++ model/dataset.py
@@ -0,0 +1,56 @@
+import warnings
+from sklearn.base import BaseEstimator, TransformerMixin
+from torchtext.data import Example, Dataset, Field, BucketIterator
+
+
+class PandasDataset(Dataset):
+    def __init__(self, df, fields):
+        proc = [df[col].apply(f.preprocess) for col, f in self.fields]
+        examples = [Example.fromlist(f, self.fields) for f in zip(*proc)]
+        super().__init__(examples, self.fields)
+
+
+class TextPreprocessor(BaseEstimator, TransformerMixin):
+    def __init__(self, fields, min_freq=1):
+        self.fields = fields
+        self.min_freq = min_freq
+
+    def fit(self, X, y=None):
+        dataset = self.transform(X, y)
+        for name, field in dataset.fields.items():
+            if field.use_vocab:
+                field.build_vocab(dataset, min_freq=self.min_freq)
+        return self
+
+    def transform(self, X, y=None):
+        with warnings.catch_warnings(record=True):
+            return PandasDataset(X, self.fields)
+
+
+def build_preprocessor(min_freq=5):
+    with warnings.catch_warnings(record=True):
+        text_field = Field(
+            tokenize=None,
+            init_token=None,
+            pad_token="<unk>",
+            unk_token="<unk>",
+            eos_token=None,
+            batch_first=True,
+            pad_first=True,
+        )
+        fields = [
+            ('text', text_field),
+            ('gold', text_field),
+        ]
+        return TextPreprocessor(fields, min_freq=min_freq)
+
+
+class SequenceIterator(BucketIterator):
+    def __init__(self, *args, **kwargs):
+        with warnings.catch_warnings(record=True):
+            super().__init__(*args, **kwargs)
+
+    def __iter__(self):
+        with warnings.catch_warnings(record=True):
+            for batch in super().__iter__():
+                yield batch.text, batch.gold.view(-1)
