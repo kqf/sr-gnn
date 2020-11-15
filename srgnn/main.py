6e990570e4e00ebdd7ce9c0b2a097bbd1067cdae
Sasha <q.dev.null@gmail.com>
2020-11-15 07:11:39 +0100

    Simplify the main module

--- srgnn/main.py
+++ srgnn/main.py
@@ -1,5 +1,4 @@
 import click
-from collections import namedtuple
 from srgnn.data import read_data, ev_data
 
 from srgnn.model import build_model
@@ -11,32 +10,8 @@ from srgnn.model import build_model
 def main(path):
     train, test, valid = read_data(path)
     data = ev_data(train["text"])
-    n_node = 43098
 
-    optf = namedtuple(
-        "opt", [
-            "hiddenSize",
-            "batchSize",
-            "nonhybrid",
-            "step",
-            "lr",
-            "l2",
-            "lr_dc_step",
-            "lr_dc"
-        ])
-
-    opt = optf(
-        hiddenSize=100,
-        batchSize=100,
-        nonhybrid=True,
-        step=1,
-        l2=1e-5,
-        lr=0.001,
-        lr_dc_step=3,
-        lr_dc=0.1
-    )
-
-    model = build_model(opt, n_node)
+    model = build_model()
     model.fit(data)
 
 
