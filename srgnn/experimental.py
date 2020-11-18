21d623d1abf9267574f7339366e822f1c9d613bb
kqf <q.dev.null@gmail.com>
2020-11-18 18:59:19 +0100

    Clean gnn cell (#14)
    * Clean the cell definition
    
    * Remove the redundant dimension usage
    
    * Simplify the gates

--- srgnn/experimental.py
+++ srgnn/experimental.py
@@ -1,5 +1,4 @@
 import torch
-from torch import nn
 from torch.nn import Parameter
 import torch.nn.functional as F
 
@@ -28,42 +27,43 @@ class GNN(torch.nn.Module):
     def __init__(self, hidden_size, step=1):
         super(GNN, self).__init__()
         self.step = step
-        self.hidden_size = hidden_size
-        self.input_size = hidden_size * 2
-        self.gate_size = 3 * hidden_size
-        self.w_ih = Parameter(torch.Tensor(self.gate_size, self.input_size))
-        self.w_hh = Parameter(torch.Tensor(self.gate_size, self.hidden_size))
-        self.b_ih = Parameter(torch.Tensor(self.gate_size))
-        self.b_hh = Parameter(torch.Tensor(self.gate_size))
-        self.b_iah = Parameter(torch.Tensor(self.hidden_size))
-        self.b_oah = Parameter(torch.Tensor(self.hidden_size))
+        input_size = hidden_size * 2
+        gate_size = 3 * hidden_size
 
-        self.linear_edge_in = nn.Linear(
-            self.hidden_size, self.hidden_size, bias=True)
-        self.linear_edge_out = nn.Linear(
-            self.hidden_size, self.hidden_size, bias=True)
-        self.linear_edge_f = nn.Linear(
-            self.hidden_size, self.hidden_size, bias=True)
+        self._igate = torch.nn.Linear(input_size, gate_size)
+        self._hgate = torch.nn.Linear(hidden_size, gate_size)
 
-    def GNNCell(self, A, hidden):
-        input_in = torch.matmul(A[:, :, :A.shape[1]],
-                                self.linear_edge_in(hidden)) + self.b_iah
+        self.b_iah = Parameter(torch.Tensor(hidden_size))
+        self.b_oah = Parameter(torch.Tensor(hidden_size))
+
+        self._ein = torch.nn.Linear(hidden_size, hidden_size, bias=True)
+        self._eou = torch.nn.Linear(hidden_size, hidden_size, bias=True)
+
+    def cell(self, A, hidden):
+        input_in = torch.matmul(
+            A[:, :, :A.shape[1]], self._ein(hidden)) + self.b_iah
+
         input_out = torch.matmul(
-            A[:, :, A.shape[1]: 2 * A.shape[1]], self.linear_edge_out(hidden)) + self.b_oah
+            A[:, :, A.shape[1]:], self._eou(hidden)) + self.b_oah
+
         inputs = torch.cat([input_in, input_out], 2)
-        gi = F.linear(inputs, self.w_ih, self.b_ih)
-        gh = F.linear(hidden, self.w_hh, self.b_hh)
+
+        gi = self._igate(inputs)
+        gh = self._hgate(hidden)
+
         i_r, i_i, i_n = gi.chunk(3, 2)
         h_r, h_i, h_n = gh.chunk(3, 2)
+
         resetgate = torch.sigmoid(i_r + h_r)
         inputgate = torch.sigmoid(i_i + h_i)
+
         newgate = torch.tanh(i_n + resetgate * h_n)
         hy = newgate + inputgate * (hidden - newgate)
         return hy
 
     def forward(self, A, hidden):
         for i in range(self.step):
-            hidden = self.GNNCell(A, hidden)
+            hidden = self.cell(A, hidden)
         return hidden
 
 
@@ -71,7 +71,7 @@ class SRGNN(torch.nn.Module):
     def __init__(self, hidden_size, vocab_size, nonhybrid=True, step=1):
         super(SRGNN, self).__init__()
         self.nonhybrid = nonhybrid
-        self._emb = nn.Embedding(vocab_size, hidden_size)
+        self._emb = torch.nn.Embedding(vocab_size, hidden_size)
         self._gnn = GNN(hidden_size, step=step)
         self._fc1 = torch.nn.Linear(hidden_size, hidden_size)
         self._fc2 = torch.nn.Linear(hidden_size, hidden_size)
