1fc6d8098df61dd948eafc1821fde5d19b9b0436
Sasha <q.dev.null@gmail.com>
2020-11-20 16:42:31 +0100

    Add train-set evaluation

--- srgnn/main.py
+++ srgnn/main.py
@@ -11,12 +11,14 @@ def main(path):
     train, test, valid = read_data(path)
     X = ev_data(train["text"])
     X_val = ev_data(train["text"])
+    X_te = ev_data(train["test"])
 
     model = build_model(X_val=X_val)
     model.fit(X)
 
     evaluate(model, X, "train")
     evaluate(model, X_val, "valid")
+    evaluate(model, X_te, "test")
 
 
 if __name__ == '__main__':
